874ad07eb160c61cb38d0cc258d7df7b88ed7c0e
josephagregoryii <38510280+josephagregoryii@users.noreply.github.com>
2018-04-18 17:51:26 -0700

    commit 1

--- js/main.js
+++
@@ -1,9 +0,0 @@
-var h1 = document.querySelector("h1");
-var succeeded = document.getElementsByClassName("succeeded");
-
-if (succeeded.textContent == "1") {
-succeeded.textContent = "Yes"
-}
-} else {
-    succeeded.textContent = "No"
-}
